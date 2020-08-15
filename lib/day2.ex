0a6b4c3fa67dfaa091b380e3187572adc47327f3
Serafeim Papastefanos <spapas@gmail.com>
2020-08-15 18:55:58 +0300

    Finish day3b

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,21 +1,27 @@
 defmodule Day2 do
   def runner(progr, pc) do
     op = progr |> Enum.at(pc)
+
     case op do
-      99 -> progr
-      1 -> p1 = Enum.at(progr, pc+1)
-        p2 = Enum.at(progr, pc+2)
-        p3 = Enum.at(progr, pc+3)
+      99 ->
+        progr
+
+      1 ->
+        p1 = Enum.at(progr, pc + 1)
+        p2 = Enum.at(progr, pc + 2)
+        p3 = Enum.at(progr, pc + 3)
         new_progr = progr |> List.replace_at(p3, Enum.at(progr, p1) + Enum.at(progr, p2))
         runner(new_progr, pc + 4)
-     2 -> p1 = Enum.at(progr, pc+1)
-      p2 = Enum.at(progr, pc+2)
-      p3 = Enum.at(progr, pc+3)
-      new_progr = progr |> List.replace_at(p3, Enum.at(progr, p1) * Enum.at(progr, p2))
-      runner(new_progr, pc + 4)
 
+      2 ->
+        p1 = Enum.at(progr, pc + 1)
+        p2 = Enum.at(progr, pc + 2)
+        p3 = Enum.at(progr, pc + 3)
+        new_progr = progr |> List.replace_at(p3, Enum.at(progr, p1) * Enum.at(progr, p2))
+        runner(new_progr, pc + 4)
     end
   end
+
   def day2 do
     prog = File.read!("day2.txt") |> String.split(",") |> Enum.map(&String.to_integer/1)
     new_prog = prog |> List.replace_at(1, 12) |> List.replace_at(2, 2)
@@ -24,12 +30,15 @@ defmodule Day2 do
 
   def day2b do
     prog = File.read!("day2.txt") |> String.split(",") |> Enum.map(&String.to_integer/1)
-    pairs = for a <- 1..99, b <- 1..99, do: [a,b]
-    [[_h, [a,b]]] = Enum.map(pairs, fn [a,b]=p ->
-      new_prog = prog |> List.replace_at(1, a) |> List.replace_at(2, b)
-      [runner(new_prog, 0) |> hd, p]
-    end) |> Enum.filter(&(&1 |> hd == 19690720))
-    a*100+b
-  end
+    pairs = for a <- 1..99, b <- 1..99, do: [a, b]
 
+    [[_h, [a, b]]] =
+      Enum.map(pairs, fn [a, b] = p ->
+        new_prog = prog |> List.replace_at(1, a) |> List.replace_at(2, b)
+        [runner(new_prog, 0) |> hd, p]
+      end)
+      |> Enum.filter(&(&1 |> hd == 19_690_720))
+
+    a * 100 + b
+  end
 end
--- lib/day3.ex
+++ lib/day3.ex
@@ -1,40 +1,63 @@
 defmodule Day3 do
-
-  def get_wire_points_reducer("U"<>rest, acc) do
+  def get_wire_points_reducer("U" <> rest, acc) do
     create_wire_points_int(0, 1, rest, acc)
   end
 
-  def get_wire_points_reducer("D"<>rest, acc) do
+  def get_wire_points_reducer("D" <> rest, acc) do
     create_wire_points_int(0, -1, rest, acc)
   end
 
-  def get_wire_points_reducer("L"<>rest, acc) do
+  def get_wire_points_reducer("L" <> rest, acc) do
     create_wire_points_int(-1, 0, rest, acc)
   end
 
-  def get_wire_points_reducer("R"<>rest, acc) do
+  def get_wire_points_reducer("R" <> rest, acc) do
     create_wire_points_int(1, 0, rest, acc)
   end
 
   def create_wire_points_int(dx, dy, rest, {{x, y}, l}) do
-    n = rest |> String.to_integer
-    r = for i <- 1..n do
-      {x + dx*i, y + dy*i}
-    end
-    {List.last(r), l++r}
+    n = rest |> String.to_integer()
+
+    r =
+      for i <- 1..n do
+        {x + dx * i, y + dy * i}
+      end
+
+    {List.last(r), l ++ r}
   end
 
   def get_wire_points(w) do
-    w |> String.split(",") |> Enum.reduce({{0,0}, []}, &get_wire_points_reducer/2)
+    w |> String.split(",") |> Enum.reduce({{0, 0}, []}, &get_wire_points_reducer/2)
   end
 
-  def manhattan({x,y}), do: abs(x) + abs(y)
-  def day3 do
+  def get_points do
     [w1, w2, _] = File.read!("day3.txt") |> String.split("\n")
     {_p, points1} = w1 |> get_wire_points()
     {_p, points2} = w2 |> get_wire_points()
-    MapSet.intersection(points1|>MapSet.new, points2|>MapSet.new)
-    |> Enum.min_by(&manhattan/1) |> manhattan
+    {points1, points2}
   end
 
+  def manhattan({x, y}), do: abs(x) + abs(y)
+
+  def get_intersections(points1, points2) do
+    MapSet.intersection(points1 |> MapSet.new(), points2 |> MapSet.new())
+  end
+
+  def day3 do
+    {points1, points2} = get_points()
+    get_intersections(points1, points2) |> Enum.min_by(&manhattan/1) |> manhattan
+  end
+
+  def day3b do
+    {points1, points2} = get_points()
+
+    min_intersection =
+      get_intersections(points1, points2)
+      |> Enum.min_by(fn p ->
+        Enum.find_index(points1, &(p == &1)) + Enum.find_index(points2, &(p == &1))
+      end)
+
+    Enum.find_index(points1, &(min_intersection == &1)) +
+      Enum.find_index(points2, &(min_intersection == &1)) + 1 + 1
+  end
 end
