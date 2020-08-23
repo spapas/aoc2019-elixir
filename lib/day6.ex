6ffa3f3a44d415a69e3033206845b8f5c57c9942
Serafeim Papastefanos <spapas@gmail.com>
2020-08-23 23:51:10 +0300

    Day6 part2 ok

--- lib/day6.ex
+++ lib/day6.ex
@@ -47,6 +47,19 @@ defmodule Day6 do
     input_set |> Enum.map(&(count_distance(&1, 0, input_map))) |> Enum.sum
   end
 
+  def get_path("COM", acc, _input_map), do: ["COM" | acc]
+  def get_path(el, acc, input_map), do: get_path(Map.get(input_map, el), [el | acc], input_map)
+
   def day6b() do
+    input = read_input()
+    input_map = input |> make_input_map()
+    # Find path from YOU to COM
+    you_path = get_path("YOU", [], input_map)
+    # Find path from SAN to COM
+    san_path = get_path("SAN", [], input_map)
+    # Find intersecting path between the above two
+    path_is = MapSet.intersection(MapSet.new(you_path), MapSet.new(san_path))
+    # Just count the length of the non intersecting path
+    (you_path |> Enum.count()) + (san_path |> Enum.count()) - 2*( (path_is |> Enum.count()) +1 )
   end
 end
