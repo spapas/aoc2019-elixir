610ec8ae3558545b57bf499218d77dd20faa2021
Serafeim Papastefanos <spapas@gmail.com>
2020-08-27 13:17:23 +0300

    Start day9

---
+++ lib/day9.ex
@@ -0,0 +1,12 @@
+defmodule Day9 do
+  import Intcode, only: [runner: 2, read_program: 1]
+
+  def day9 do
+    new_prog = read_program("day9.txt")
+    runner(new_prog, 0
+  end
+
+  def day2b do
+
+  end
+end
