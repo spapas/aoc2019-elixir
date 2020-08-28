6290747144ad0f4330f94f509dbe0d4fd8775bde
Serafeim Papastefanos <spapas@gmail.com>
2020-08-28 12:57:57 +0300

    Do some tests with day 10

--- lib/day10.ex
+++ lib/day10.ex
@@ -7,6 +7,44 @@ defmodule Day10 do
     end)
   end
 
+  def get_map(grid) do
+
+    grid |> Enum.with_index() |> Enum.map( fn {line, y} ->
+      line |> Enum.with_index() |> Enum.map( fn {point, x} ->
+        #"x = #{x}, y = #{y}, is asteroid = #{point}" |> IO.inspect()
+        {{x,y}, point}
+
+      end)
+    end)
+    |> Enum.reduce([], fn l, acc -> Enum.reduce(l, acc, &[&1 | &2]) end )
+    |> Map.new
+
+  end
+
+  def get_locations_quart(rx, ry) do
+    #h = grid |> Enum.count()
+    #w = grid |> Enum.at(0) |> Enum.count()
+
+    #oob = fn(x, y) ->
+    #  x > w or x < 0 or y > h or y < 0
+    #end
+
+
+
+  end
+
+  def get_locations_around(grid, h, w, {px, py}) do
+
+
+    # TOP LEFT
+    (for x <- px..0, y <- py..0, do: {x, y}) |> Enum.sort_by(fn {x, y} -> abs(px-x)+abs(py-y) end ) |> IO.inspect
+    # TOP RIGHT
+    (for x <- px..w, y <- py..0, do: {x, y}) |> Enum.sort_by(fn {x, y} -> abs(px-x)+abs(py-y) end )  |> IO.inspect
+    # BOTTOM RIGHT
+    (for x <- px..w, y <- py..h, do: {x, y}) |> Enum.sort_by(fn {x, y} -> abs(px-x)+abs(py-y) end )  |> IO.inspect
+    # BOTTOM LEFT
+    (for x <- px..0, y <- py..h, do: {x, y}) |> Enum.sort_by(fn {x, y} -> abs(px-x)+abs(py-y) end )  |> IO.inspect
+  end
 
 
   def day10 do
@@ -14,17 +52,18 @@ defmodule Day10 do
     h = grid |> Enum.count()
     w = grid |> Enum.at(0) |> Enum.count()
 
-    grid |> Enum.with_index() |> Enum.map( fn {line, y} ->
-      line |> Enum.with_index() |> Enum.map( fn {point, x} ->
-        "x = #{x}, y = #{y}, is asteroid = #{point}" |> IO.inspect()
-
-      end)
-    end)
-
+    mapgrid = get_map(grid)
 
   end
 
   def day10b do
 
   end
+
+  def pranges(x,y, x, y, acc), do: acc |> Enum.reverse
+  def pranges(x,y, lx, ly, acc) when x == y, do: pranges(x+1, y, lx, ly, [{x, y} | acc])
+  def pranges(x,y, lx, ly, acc) when x > y, do: pranges(x, y+1, lx, ly, [{x-1, y+1} | acc])
+  #def pranges(x,y, lx, ly, acc), do: pranges(x-1, y+1, lx, ly, [{x-1, y} | acc])
+  #def pranges(x,y, lx, ly, acc) when x < y, do: pranges(x+1, y, lx, ly, [{x, y} | acc])
+
 end
