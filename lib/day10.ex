1ecc1b3621ca5b7c22915902d22e997d8296b3a9
Serafeim Papastefanos <spapas@gmail.com>
2020-08-30 14:32:25 +0300

    Some more changes

--- lib/day10.ex
+++ lib/day10.ex
@@ -1,6 +1,5 @@
 defmodule Day10 do
 
-
   def read_input() do
     File.read!("day10.txt") |> String.split() |> Enum.map( fn el ->
       el |> String.graphemes() |> Enum.map(&(if &1=="#", do: 1, else: 0))
@@ -42,23 +41,56 @@ defmodule Day10 do
     (for y <- (py-1)..0, do: {0, y}) |> location_sort(p)
   end
   def get_locations_tl({px, py}=p) do
-    (for x <- (px-1)..0, y <- (py-1)..0, do: {x, y}) |> location_sort(p)
+    (for x <- px..0, y <- (py-1)..0, do: {x, y}) |> location_sort(p)
+  end
+
+  def get_locations_tr({px, py}, w) when px == w-1 and py==0, do: []
+  def get_locations_tr({px, py}=p, w) when py == 0 do
+    (for x <- (px+1)..(w-1), do: {x, 0}) |> location_sort(p)
+  end
+  def get_locations_tr({px, py}=p, w) when px == w-1 do
+    (for y <- (py-1)..0, do: {w-1, y}) |> location_sort(p)
+  end
+  def get_locations_tr({px, py}=p, w) do
+    (for x <- (px+1)..(w-1), y <- py..0, do: {x, y}) |> location_sort(p)
+  end
+
+  def get_locations_br({px, py}, w, h) when px == w-1 and py==h-1, do: []
+  def get_locations_br({px, py}=p, w, h) when py == h-1 do
+    (for x <- (px+1)..(w-1), do: {x, h-1}) |> location_sort(p)
+  end
+  def get_locations_br({px, py}=p, w, h) when px == w-1 do
+    (for y <- (py+1)..(h-1), do: {w-1, y}) |> location_sort(p)
+  end
+  def get_locations_br({px, py}=p, w, h) do
+    (for x <- px..(w-1), y <- (py+1)..(h-1), do: {x, y}) |> location_sort(p)
+  end
+
+  def get_locations_bl({px, py}, h) when px == 0 and py==(h-1), do: []
+  def get_locations_bl({px, py}=p, h) when py == (h-1) do
+    (for x <- (px-1)..0, do: {x, h-1}) |> location_sort(p)
+  end
+  def get_locations_bl({px, py}=p, h) when px == 0 do
+    (for y <- py..(h-1), do: {0, y}) |> location_sort(p)
+  end
+  def get_locations_bl({px, py}=p, h) do
+    (for x <- (px-1)..0, y <- py..(h-1), do: {x, y}) |> location_sort(p)
   end
 
   def get_locations_around(grid, h, w, {px, py}) do
 
     # TOP RIGHT
     "TR"|>IO.puts
-    (for x <- px..(w-1), y <- (py-1)..0, do: {x, y}) |> Enum.sort_by(fn {x, y} -> abs(px-x)+abs(py-y) end )  |> IO.inspect
+    (for x <- (px+1)..(w-1), y <- py..0, do: {x, y}) |> Enum.sort_by(fn {x, y} -> abs(px-x)+abs(py-y) end )  |> IO.inspect
     # BOTTOM RIGHT
     "BR"|>IO.puts
-    (for x <- px..(w-1), y <- py..(h-1), do: {x, y}) |> Enum.sort_by(fn {x, y} -> abs(px-x)+abs(py-y) end )  |> IO.inspect
+    (for x <- px..(w-1), y <- (py+1)..(h-1), do: {x, y}) |> Enum.sort_by(fn {x, y} -> abs(px-x)+abs(py-y) end )  |> IO.inspect
     # BOTTOM LEFT
     "BL"|>IO.puts
     (for x <- (px-1)..0, y <- py..(h-1), do: {x, y}) |> Enum.sort_by(fn {x, y} -> abs(px-x)+abs(py-y) end )  |> IO.inspect
     # TOP LEFT
     "TL"|>IO.puts
-    (for x <- (px-1)..0, y <- (py-1)..0, do: {x, y}) |> Enum.sort_by(fn {x, y} -> abs(px-x)+abs(py-y) end ) |> IO.inspect
+    (for x <- px..0, y <- (py-1)..0, do: {x, y}) |> Enum.sort_by(fn {x, y} -> abs(px-x)+abs(py-y) end ) |> IO.inspect
     "~"
   end
 
@@ -83,3 +115,7 @@ defmodule Day10 do
   #def pranges(x,y, lx, ly, acc) when x < y, do: pranges(x+1, y, lx, ly, [{x, y} | acc])
 
 end
+
+###
+# #
+###
