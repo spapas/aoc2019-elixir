e47c96542d69e752daa2982eb146c611c2f517f6
Serafeim Papastefanos <spapas@gmail.com>
2021-09-23 22:45:14 +0300

    More things

--- lib/day18.ex
+++ lib/day18.ex
@@ -5,13 +5,15 @@ defmodule Day18 do
   end
 
   def input_to_map(input) do
-    input |> String.split("\n") |> Enum.map( &String.graphemes/1) |> Enum.filter(&(Enum.count(&1)>0))
+    input |> String.split("\r\n") |> Enum.map( &String.graphemes/1) |> Enum.filter(&(Enum.count(&1)>0))
     |> Enum.with_index |> Enum.reduce(%{}, fn {v, y}, accy ->
       v |> Enum.with_index() |> Enum.reduce(accy, fn {v, x}, accx ->
         Map.put(accx, {x, y}, v)
       end)
     end)
   end
+
+  def map(), do: input_to_map(read_input())
 
 
 
@@ -21,7 +23,7 @@ defmodule Day18 do
   end
 
   def get_initial_state(mm) do
-    st = get_start(mm)
+    pos = get_start(mm)
     %{
       pos: pos,
       steps: 0,
@@ -29,11 +31,41 @@ defmodule Day18 do
     }
   end
 
+  def get_next_state(%{pos: {x, y}, steps: steps, keys: keys}, mm) do
+    case Map.get(mm, {x, y}) do
+      v when v in [".", "@"] -> %{pos: {x, y}, steps: steps + 1, keys: keys}
+      v when v >= "a" and v <= "z" -> %{pos: {x, y}, steps: steps + 1, keys: MapSet.put(keys, v)}
+      v when v >= "A" and v <= "Z" -> if String.downcase(v) in keys do
+          %{pos: {x, y}, steps: steps + 1, keys: keys}
+        else
+          nil
+        end
+      _ -> nil
+    end
+  end
+
   def get_next_states(%{pos: {x, y}, steps: steps, keys: keys}, mm) do
     p1 = {x - 1, y}
     p2 = {x + 1, y}
     p3 = {x, y - 1}
     p4 = {x, y + 1}
+    [p1, p2, p3, p4]
+    |> Enum.map(fn p ->
+      get_next_state(%{ pos: p, steps: steps, keys: keys }, mm)
+    end)
+    |> Enum.filter(fn s -> s != nil end)
+  end
+
+  def get_doors(mm) do
+    mm |>  Map.values |> MapSet.new |>  Enum.filter( &(&1>="A" and &1<="Z")) |> Enum.sort
+  end
+
+  def get_keys(mm) do
+    mm |>  Map.values |> MapSet.new |> Enum.filter( &(&1>="a" and &1<="z")) |> Enum.sort
+  end
+
+  def get_other_things(mm) do
+    mm |>  Map.values |> MapSet.new |> Enum.filter( &( (&1<"a" or &1 >"z") and ( &1<"A" or &1 >"Z")))
   end
 
 
