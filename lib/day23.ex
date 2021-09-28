4436cbc8667508acb7bc858623c948828b7ff231
Serafeim Papastefanos <spapas@gmail.com>
2021-09-28 11:41:04 +0300

    Day23

--- lib/day23.ex
+++ lib/day23.ex
@@ -33,45 +33,63 @@ defmodule Day23 do
     :queue.is_empty(q)
   end
 
+  def remove_255(q) do
+    :queue.filter( fn [addr, x, y] -> addr != 255 end, q)
+  end
 
-  def init_queue(network) do
-    queue = get_queue()
 
-    0..49 |> Enum.reduce({network, queue}, fn addr, {n, q} -> send_packet(addr, [-1], n, q) end)
+
+  def init_queue(network,queue, nat ) do
+
+
+    0..49 |> Enum.reduce({network, queue, nat}, fn addr, {n, q, nat} -> send_packet(addr, [-1], n, q, nat) end)
 
   end
 
 
 
-  def send_packet(idx, data, network, q) do
+  def send_packet(idx, data, network, q, nat) do
     {progr, pc, options} = Map.get(network, idx)
     {:block, progr, pc, options} = runner(progr, pc, Keyword.put(options, :input, data))
     network = Map.put(network, idx, {progr, pc, options})
     output = Keyword.get(options, :output, [])
-    q = output |> Enum.reverse |> Enum.chunk_every(3) |> Enum.reduce(q, fn (el, q) -> push(q, el) end )
-    {network, q}
+    chunked_output = output |> Enum.reverse |> Enum.chunk_every(3)
+    nat = case Enum.filter(Enum.reverse(chunked_output), fn [a, x, y] -> a == 255 end) do
+      [] -> nat
+      [h|_t] -> h
+    end
+    q = chunked_output |> Enum.filter(fn [a, x, y] -> a != 255 end) |> Enum.reduce(q, fn (el, q) -> push(q, el) end )
+    {network, q, nat}
   end
 
-  def step(network, queue) do
-    {curr, q} = pop(queue) |> IO.inspect
-    case curr do
-      [255, data] -> data |> IO.inspect
-      [idx, -1] -> send_packet(idx, [-1], network, q)
-      [idx, x, y] -> send_packet(idx, [x,y], network, q)
+  def step(network, queue, nat ) do
+    if empty?(queue) do
+      nat |> IO.inspect
+
+      send_packet(0, nat, network, queue, nil)
+
+    else
+      {curr, q} = pop(queue)
+      case curr do
+        [255, x, y] -> 55
+        [idx, -1] -> send_packet(idx, [-1], network, q, nat)
+        [idx, x, y] -> send_packet(idx, [x,y], network, q, nat)
+      end
     end
   end
 
-  def runme(network, queue) do
-    case step(network, queue) do
-      {network, queue} -> runme(network, queue)
+  def runme(network, queue, nat) do
+    case step(network, queue, nat) do
+      {network, queue, nat} -> runme(network, queue, nat)
       data -> data
     end
   end
 
-  def part1() do
+  def part2() do
     network = get_network()
-    {network, queue} = init_queue(network)
-    runme(network, queue)
+    {network, queue, nat} = init_queue(network, get_queue, nil)
+
+    runme(network, queue, nat)
   end
 
 end
